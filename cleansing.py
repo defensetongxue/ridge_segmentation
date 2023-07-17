23fb55106e97afaddc0fc311b1d4035b8b1d4d57
guoyi <1900013009@pku.edu.cn>
2023-07-17 13:06:32 +0800

    update add new branch

--- cleansing.py
+++ cleansing.py
@@ -39,54 +39,94 @@ def generate_segmentation_mask(data_path, patch_size, stride):
     splits=['train','val']
     for split in splits:
         with open(os.path.join(data_path,'ridge',f'{split}.json'),'r') as f:
+            ridge_list=json.load(f)
+        with open(os.path.join(data_path,'annotations',f'{split}.json'),'r') as f:
             data_list=json.load(f)
-
+        
+        ridge_dict={i['image_name']:i for i in ridge_list}
         annotate=[]
-        for  data in data_list:
-            mask = Image.open(data['diffusion_mask_path'])
-            mask_tensor=torch.from_numpy(np.array(mask,np.float32, copy=False))
-            mask_tensor[mask_tensor!=0]=1
+        for  data_item in data_list:
+            if data_item['image_name'] in ridge_dict:
+                data= ridge_dict[data_item['image_name']]
+                mask = Image.open(data['diffusion_mask_path'])
+                mask_tensor=torch.from_numpy(np.array(mask,np.float32, copy=False))
+                mask_tensor[mask_tensor!=0]=1
 
-            # Load image, position_embed and mask
-            img = Image.open(data['image_path']).convert("RGB")
-            img_tensor = transforms.ToTensor()(img)
+                # Load image, position_embed and mask
+                img = Image.open(data['image_path']).convert("RGB")
+                img_tensor = transforms.ToTensor()(img)
 
-            pos_path=os.path.join(data_path,'pos_embed',data['image_name'].split('.')[0]+'.pt')
-            pos_embed=torch.load(pos_path)
-            pos_embed=F.interpolate(pos_embed[None,None,:,:], size=mask_tensor.shape, mode='nearest')
-            pos_embed=pos_embed.squeeze()
-            # Unfold to patches
-            patches_img = img_tensor.unfold(1, patch_size, stride).unfold(2, patch_size, stride)
-            patches_mask = mask_tensor.unfold(0, patch_size, stride).unfold(1, patch_size, stride)
-            pos_embed_patches=pos_embed.unfold(0, patch_size, stride).unfold(1, patch_size, stride) 
-            # Loop through all patches
-            for i in range(patches_img.shape[1]):
-                for j in range(patches_img.shape[2]):
-                    patch_img = patches_img[:,i,j].permute(1, 2, 0).numpy()
-                    patch_mask = patches_mask[i,j].numpy()
-                    pos_embed_patch=pos_embed_patches[i,j].numpy()
-                    # Save image patch
-                    patch_name=f"{data['image_name'].split('.')[0]}_{i}_{j}"
-                    img_path = os.path.join(data_path, 'ridge_seg', 'images', f"{patch_name}.jpg")
-                    Image.fromarray((patch_img * 255).astype(np.uint8)).save(img_path)
+                pos_path=os.path.join(data_path,'pos_embed',data['image_name'].split('.')[0]+'.pt')
+                pos_embed=torch.load(pos_path)
+                pos_embed=F.interpolate(pos_embed[None,None,:,:], size=mask_tensor.shape, mode='nearest')
+                pos_embed=pos_embed.squeeze()
+                # Unfold to patches
+                patches_img = img_tensor.unfold(1, patch_size, stride).unfold(2, patch_size, stride)
+                patches_mask = mask_tensor.unfold(0, patch_size, stride).unfold(1, patch_size, stride)
+                pos_embed_patches=pos_embed.unfold(0, patch_size, stride).unfold(1, patch_size, stride) 
+                # Loop through all patches
+                for i in range(patches_img.shape[1]):
+                    for j in range(patches_img.shape[2]):
+                        patch_img = patches_img[:,i,j].permute(1, 2, 0).numpy()
+                        patch_mask = patches_mask[i,j].numpy()
+                        pos_embed_patch=pos_embed_patches[i,j].numpy()
+                        # Save image patch
+                        patch_name=f"{data['image_name'].split('.')[0]}_{i}_{j}"
+                        img_path = os.path.join(data_path, 'ridge_seg', 'images', f"{patch_name}.jpg")
+                        Image.fromarray((patch_img * 255).astype(np.uint8)).save(img_path)
 
-                    # Save mask patch
-                    mask_path = os.path.join(data_path, 'ridge_seg', 'masks', f"{patch_name}.png")
-                    Image.fromarray((patch_mask * 255).astype(np.uint8)).save(mask_path)
+                        # Save mask patch
+                        mask_path = os.path.join(data_path, 'ridge_seg', 'masks', f"{patch_name}.png")
+                        Image.fromarray((patch_mask * 255).astype(np.uint8)).save(mask_path)
 
-                    # Save pos embed
-                    pos_embed_path=os.path.join(data_path,'ridge_seg','position_embed',f'{patch_name}.png')
-                    Image.fromarray((pos_embed_patch* 255).astype(np.uint8)).save(pos_embed_path)
-                    # Get class
-                    class_annote = data["class"] if patch_mask.max() > 0 else 0
+                        # Save pos embed
+                        pos_embed_path=os.path.join(data_path,'ridge_seg','position_embed',f'{patch_name}.png')
+                        Image.fromarray((pos_embed_patch* 255).astype(np.uint8)).save(pos_embed_path)
+                        # Get class
+                        class_annote = data["class"] if patch_mask.max() > 0 else 0
 
-                    annotate.append({
-                        "img_path": img_path,
-                        "mask_path": mask_path,
-                        'pos_embed_path':pos_embed_path,
-                        "class": class_annote
-                    })
+                        annotate.append({
+                            "img_path": img_path,
+                            "mask_path": mask_path,
+                            'pos_embed_path':pos_embed_path,
+                            "class": class_annote
+                        })
+            else:
+                data=data_item
+                img = Image.open(data['image_path']).convert("RGB")
+                img_tensor = transforms.ToTensor()(img)
+                pos_path=os.path.join(data_path,'pos_embed',data['image_name'].split('.')[0]+'.pt')
+                pos_embed=torch.load(pos_path)
+                pos_embed=F.interpolate(pos_embed[None,None,:,:], size=img_tensor.shape, mode='nearest')
+                pos_embed=pos_embed.squeeze()
+                patches_img = img_tensor.unfold(1, patch_size, stride).unfold(2, patch_size, stride)
+                pos_embed_patches=pos_embed.unfold(0, patch_size, stride).unfold(1, patch_size, stride) 
+               
+                for i in range(patches_img.shape[1]):
+                    for j in range(patches_img.shape[2]):
+                        patch_img = patches_img[:,i,j].permute(1, 2, 0).numpy()
+                        pos_embed_patch=pos_embed_patches[i,j].numpy()
+                        # Save image patch
+                        patch_name=f"{data['image_name'].split('.')[0]}_{i}_{j}"
+                        img_path = os.path.join(data_path, 'ridge_seg', 'images', f"{patch_name}.jpg")
+                        Image.fromarray((patch_img * 255).astype(np.uint8)).save(img_path)
 
+                        # Save mask patch
+                        mask_path = os.path.join(data_path, 'ridge_seg', 'masks', f"{patch_name}.png")
+                        Image.fromarray((torch.zeros_like(patch_img)).astype(np.uint8)).save(mask_path)
+
+                        # Save pos embed
+                        pos_embed_path=os.path.join(data_path,'ridge_seg','position_embed',f'{patch_name}.png')
+                        Image.fromarray((pos_embed_patch* 255).astype(np.uint8)).save(pos_embed_path)
+                        # Get class
+                        class_annote = 0
+
+                        annotate.append({
+                            "img_path": img_path,
+                            "mask_path": mask_path,
+                            'pos_embed_path':pos_embed_path,
+                            "class": class_annote
+                        })
         with open(os.path.join(data_path, 'ridge_seg', 'annotations', f'{split}.json'), 'w') as f:
             json.dump(annotate, f)
 
