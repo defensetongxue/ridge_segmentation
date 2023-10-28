6e3bc2e218cd1fc217e820cf8a575c7a99069e0c
defense <1900013009@pku.edu.cn>
2023-10-28 17:57:24 +0800

    add

--- utils_/ridge_diffusion.py
+++ utils_/ridge_diffusion.py
@@ -16,6 +16,8 @@ def generate_ridge_diffusion(data_path):
         data_list=json.load(f)
     for image_name in data_list:
         data=data_list[image_name]
+        if not 'ridge' in data:
+            continue
         mask = generate_diffusion_heatmap(data['image_path']
                                           ,data['ridge']['ridge_coordinate'], 
                                           factor=0.5,
@@ -45,8 +47,10 @@ def generate_diffusion_heatmap(image_path,points,factor=0.5,Gauss=False):
         new_points.append([int(x*factor),int(y*factor)])
     points=np.array(new_points)
 
-    heatmap = generate_heatmap(heatmap,img_tensor,points)
+    heatmap = generate_heatmap(heatmap,img_tensor,points,image_path)
     if Gauss:
+        # as we are using bce loss as loss function, no 
+        # gauss map is needed
         Gauss_heatmap=gaussian_filter(heatmap,3)
         heatmap=np.where(heatmap>Gauss_heatmap,heatmap,Gauss_heatmap)
     mask=heatmap2mask(heatmap,int(1/factor))
@@ -101,24 +105,65 @@ def imge_tensor_compress(img_path,factor):
     img=img.squeeze()
     return img
 
-def generate_heatmap(heatmap,img_tensor, points):
+def generate_heatmap(heatmap,img_tensor, points,image_path):
     points=generate_point_sequence(points)
     for i in range(points.shape[0]-1):
-        heatmap=diffusion(heatmap,img_tensor,points[i],points[i+1])
+        heatmap=diffusion(heatmap,img_tensor,points[i],points[i+1],image_path)
 
     return heatmap
 
 def generate_point_sequence(points):
-    points = points[points[:,0].argsort()] if (max(points[:,0]) - min(points[:,0])) > (max(points[:,1]) - min(points[:,1])) else points[points[:,1].argsort()]
-    return points
-
+    """
+    Generate a sequence of points ordered based on proximity, starting from
+    the point with the smallest x or y value.
+    
+    points: np.array, shape (n_points, 2)
+    
+    Returns: np.array, shape (n_points, 2)
+    """
+    # Decide axis
+    x_range = np.ptp(points[:, 0])  # Peak to peak (max - min) for x
+    y_range = np.ptp(points[:, 1])  # Peak to peak (max - min) for y
+    
+    # Sort by x or y depending on range
+    if x_range > y_range:
+        axis = 0  # x-axis
+    else:
+        axis = 1  # y-axis
+    
+    # Find the starting point (x0 or y0)
+    start_idx = np.argmin(points[:, axis])
+    start_point = points[start_idx]
+    
+    # Initialize sequence with the starting point
+    sequence = [start_point]
+    
+    # Initialize set of unvisited points
+    unvisited = set(range(len(points)))
+    unvisited.remove(start_idx)
+    
+    # Greedy algorithm to find the closest points
+    current_point = start_point
+    while unvisited:
+        # Find the closest point to the current point
+        next_idx = min(unvisited, key=lambda idx: get_distance(current_point, points[idx]))
+        next_point = points[next_idx]
+        
+        # Update current point and sequence
+        current_point = next_point
+        sequence.append(current_point)
+        
+        # Remove the next point from the unvisited set
+        unvisited.remove(next_idx)
+    
+    return np.array(sequence)
 def get_distance(p0, p1):
     return ((p0[0]-p1[0])**2 + (p0[1]-p1[1])**2)**0.5
 def get_similarity(img_tensor,p0,p1):
     return 1-((img_tensor[p0[1],p0[0]]-img_tensor[p1[1],p1[0]])**2)
 
 
-def diffusion(heatmap,img_tensor,p0,p1):
+def diffusion(heatmap,img_tensor,p0,p1,image_path):
     heatmap[p0[1],p0[0]]=1
     heatmap[p1[1],p1[0]]=1
     px=p0
@@ -139,6 +184,7 @@ def diffusion(heatmap,img_tensor,p0,p1):
                     max_simi=simi
                     rem=[i,j]
         if len(rem)==0:
+            print(image_path)
             return heatmap
         px=[px[0]+rem[0],px[1]+rem[1]]
         now_distance=get_distance(px,p1)
