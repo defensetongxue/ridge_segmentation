175adb647b713c232b51611886d5a2ea0443c1ec
guoyi <1900013009@pku.edu.cn>
2023-11-25 01:16:10 +0800

    update

--- todo.sh
+++ todo.sh
@@ -3,9 +3,10 @@ python -u train.py
 python segment_test.py 
 cd ../SentenceROP
 python cleansing.py
-python -u train.py --lr 5e-4 --wd 5e-4
-python -u train.py --lr 1e-3 --wd 5e-4
-python -u train.py --lr 5e-4 --wd 5e-3
-python -u train.py --lr 5e-4 --wd 5e-2
+python -u train.py --aux_r 0.0
+python -u train.py --aux_r 0.2
+python -u train.py --aux_r 0.5
+python -u train.py --aux_r 0.8
+python -u train.py --aux_r 1.0
 python ring.py
 shutdown
