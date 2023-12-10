27cb817d65e698376a4633756d9066edfd8ff3bb
guoyi <1900013009@pku.edu.cn>
2023-12-11 00:39:38 +0800

    bug

--- todo.sh
+++ todo.sh
@@ -1,4 +1,4 @@
-python  -u cleansing.py --split_name clr_1
+# python  -u cleansing.py --split_name clr_1
 python -u train.py --split_name clr_1
 python -u test.py --split_name clr_1
 python  ring.py 
