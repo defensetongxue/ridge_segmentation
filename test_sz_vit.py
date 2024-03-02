f5a2a0e7bc168bb60d422735fedbea8ac8af06ec
guoyi <1900013009@pku.edu.cn>
2024-03-02 15:09:20 +0800

    update

--- test_sz_vit.py
+++ test_sz_vit.py
@@ -11,7 +11,7 @@ from torchvision import transforms
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
 args = get_config()
-
+args.data_path='../autodl-tmp/ROP_shen'
 # Init the result file to store the pytorch model and other mid-result
 result_path = args.result_path
 os.makedirs(result_path,exist_ok=True)
@@ -25,7 +25,7 @@ model.load_state_dict(
     torch.load(os.path.join(args.save_dir,f"{args.split_name}_{args.configs['save_name']}")))
 print("load the checkpoint in {}".format(os.path.join(args.save_dir,f"{args.split_name}_{args.configs['save_name']}")))
 model.eval()
-# args.split_name='all'
+args.split_name='all'
 # Test the model and save visualizations
 with open(os.path.join(args.data_path,'split',f'{args.split_name}.json'),'r') as f:
     split_list=json.load(f)['test']
@@ -100,8 +100,8 @@ print(f"Recall: {recall:.4f}")
 # Check if the record file exists and load it; if not, initialize an empty dict
 
 
-record_path = './experiments/record_fs.json'
-# record_path = './experiments/shen_record.json'
+# record_path = './experiments/record_fs.json'
+record_path = './experiments/shen_record.json'
 if os.path.exists(record_path):
     with open(record_path, 'r') as f:
         record = json.load(f)
--- todo.sh
+++ todo.sh
@@ -1,16 +1,11 @@
 
 # # python generate_mask.py
-python -u test.py --split_name clr_1 --cfg ./config_file/hrnet_w48.json   
-# python -u test.py --split_name clr_2 --cfg ./config_file/hrnet_w48.json   
-# python -u test.py --split_name clr_3 --cfg ./config_file/hrnet_w48.json   
-# python -u test.py --split_name clr_4 --cfg ./config_file/hrnet_w48.json   
-python -u test.py --split_name clr_1 --cfg ./config_file/unet_small.json   
-# python -u test.py --split_name clr_2 --cfg ./config_file/unet_small.json   
-# python -u test.py --split_name clr_3 --cfg ./config_file/unet_small.json   
-# python -u test.py --split_name clr_4 --cfg ./config_file/unet_small.json   
-python -u test_vit.py  --split_name clr_1 --cfg ./config_file/transunet.json
-# python -u test_vit.py  --split_name clr_2 --cfg ./config_file/transunet.json
-# python -u test_vit.py  --split_name clr_3 --cfg ./config_file/transunet.json
-# python -u test_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json
-cd ../ROP_BaseLine/
-sh todo.sh
+python -u train_vit.py  --split_name clr_1 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u train_vit.py  --split_name clr_2 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u train_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u train_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u test_vit.py  --split_name clr_1 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u test_vit.py  --split_name clr_2 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u test_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u test_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u test_sz_vit.py  --split_name clr_1 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
