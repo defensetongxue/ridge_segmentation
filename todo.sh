394c02830ad05f5b850ba7cea146aa6e38f93eed
guoyi <1900013009@pku.edu.cn>
2024-03-13 20:29:04 +0800

    add

--- todo.sh
+++ todo.sh
@@ -1,11 +1,5 @@
 
 # # python generate_mask.py
-python -u train_vit.py  --split_name clr_1 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u train_vit.py  --split_name clr_2 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u train_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u train_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u test_vit.py  --split_name clr_1 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u test_vit.py  --split_name clr_2 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u test_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u test_vit.py  --split_name clr_4 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
-python -u test_sz_vit.py  --split_name clr_1 --cfg ./config_file/transunet.json --lr 1e-4 --wd 5e-4
+python -u tmp.py --split_name clr_2
+python -u tmp.py --split_name clr_3
+python -u tmp.py --split_name clr_4
