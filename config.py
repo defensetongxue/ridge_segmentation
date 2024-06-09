6a9f344eccf104d2529a37c497f4a86d89f2f17f
guoyi <1900013009@pku.edu.cn>
2024-06-09 20:29:16 +0800

    update

--- config.py
+++ config.py
@@ -1,44 +1,50 @@
-import argparse,json
+import argparse
+import json
 
 def get_config():
     parser = argparse.ArgumentParser()
+    
     # cleansing
     parser.add_argument('--data_path', type=str, default='../autodl-tmp/dataset_ROP',
-                        help='Path to the target folder to store the processed datasets.')
-    parser.add_argument('--generate_ridge_diffusion', type=bool, default=False,
-                        help='if generate the ridge cooridinate from json src.')
-    parser.add_argument('--generate_mask', type=bool, default=False,
-                        help='if generate the ridge cooridinate from json src.')
+                        help='Path to the directory where the processed datasets are stored.')
+    parser.add_argument('--generate_ridge_diffusion', action='store_true',
+                        help='Flag to indicate if ridge coordinates should be generated from the source JSON file.')
+    parser.add_argument('--generate_mask', action='store_true',
+                        help='Flag to indicate if masks should be generated. this will crop the orignal ridge diffusion to patchtigy image.')
     
     # split
     parser.add_argument('--split_name', type=str, default='clr_1',
-                        help='which split to use.')
+                        help='Specify the dataset split to use for training and evaluation.')
     
     # Model
     parser.add_argument('--patch_size', type=int, default=400,
-                        help='Name of the model architecture to be used for training.')
+                        help='Size of the patches extracted from the images.')
     parser.add_argument('--stride', type=int, default=200,
-                        help='Name of the model architecture to be used for training.')
+                        help='Stride used for extracting patches from the images.')
     parser.add_argument('--ridge_seg_number', type=int, default=8,
-                        help='Name of the model architecture to be used for training.')
+                        help='Number of segments for ridge detection.')
+    
     # train and test
     parser.add_argument('--save_dir', type=str, default="./checkpoints",
-                        help='Name of the file to save the best model during training.')
+                        help='Directory to save the best model during training.')
     parser.add_argument('--result_path', type=str, default="experiments",
-                        help='Path to the visualize result or the pytorch model will be saved.')
+                        help='Path to save the visualization results or the trained PyTorch model.')
     parser.add_argument('--from_checkpoint', type=str, default="",
-                        help='load the exit checkpoint.')
+                        help='Path to an existing checkpoint to resume training from.')
     
     parser.add_argument('--lr', type=float, default=5e-4,
-                        help='load the exit checkpoint.')
+                        help='Learning rate for the optimizer.')
     parser.add_argument('--wd', type=float, default=5e-4,
-                        help='load the exit checkpoint.')
+                        help='Weight decay for the optimizer.')
+    
     # config file 
-    parser.add_argument('--cfg', help='experiment configuration filename',
-                        default="./config_file/hrnet_w48.json", type=str)
+    parser.add_argument('--cfg', type=str, default="./config_file/hrnet_w48.json",
+                        help='Path to the experiment configuration file in JSON format.')
     
     args = parser.parse_args()
+    
     # Merge args and config file 
-    with open(args.cfg,'r') as f:
-        args.configs=json.load(f)
+    with open(args.cfg, 'r') as f:
+        args.configs = json.load(f)
+    
     return args
